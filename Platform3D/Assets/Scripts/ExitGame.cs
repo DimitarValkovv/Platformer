63c746ac9286ba04e508fc02150082055b6d3965
Dimitar Valkov <mitaka_1911@abv.bg>
2021-06-07 19:03:24 +0300

    Work for 7.6.2021
    1.Menu creation

---
+++ Platform3D/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public GameObject fadeIn;
+    public GameObject thePlayer;
+    public AudioSource mainMusic;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(ExitTheGame());
+
+    }
+
+    IEnumerator ExitTheGame()
+    {
+        fadeIn.GetComponent<Animator>().Play("FadeIn");
+        thePlayer.SetActive(false);
+        mainMusic.Stop();
+        yield return new WaitForSeconds(4);
+        Application.Quit();
+        Debug.Log("You have quit the game");
+    }
+}
---
+++ Platform3D/Assets/Scripts/MenuToLevelSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuToLevelSelect : MonoBehaviour
+{
+    public GameObject fadeIn;
+    public GameObject thePlayer;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(MoveToSelectMenu());
+
+    }
+
+    IEnumerator MoveToSelectMenu()
+    {
+        fadeIn.GetComponent<Animator>().Play("FadeIn");
+        thePlayer.SetActive(false);
+        yield return new WaitForSeconds(4);
+        thePlayer.SetActive(true);
+        SceneManager.LoadScene(5);
+    }
+}
